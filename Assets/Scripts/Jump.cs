73eaff6d79a46e48d29e502cc02e59a1a52d8595
Dhrubajit <dhrubajit1992@gmail.com>
2016-05-02 16:39:57 +0800

    cube moves with click

---
+++ Assets/Scripts/Jump.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Collider))]
+public class Jump : MonoBehaviour {
+
+	private Vector3 startingPostition;
+	// Use this for initialization
+	void Start () {
+		startingPostition = transform.localPosition;
+	}
+
+	void Update(){
+		print (startingPostition);
+		if (Input.GetButtonDown("Fire1")){
+			transform.localPosition = new Vector3 (4f, 1.7f, 5f);
+		}
+	}
+
+}
