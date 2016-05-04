666b93da55eaeae6713b2f0cfe609d88903ebe60
Dhrubajit <dhrubajit1992@gmail.com>
2016-05-04 15:05:20 +0800

    added radial loading bar in a seperate scene

---
+++ Assets/Cardboard/Scripts/RadialProgressBar.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class RadialProgressBar : MonoBehaviour {
+
+
+	public Transform LoadingBar;
+
+	[SerializeField] private float currentAmount;
+	[SerializeField] private float speed;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (currentAmount < 100) {
+			currentAmount += speed * Time.deltaTime;
+		}
+		LoadingBar.GetComponent<Image> ().fillAmount = currentAmount / 100;
+	
+	}
+}
