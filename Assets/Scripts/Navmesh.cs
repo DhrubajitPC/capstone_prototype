449160af2a55fc5cecca26718673d77763fd7757
Dhrubajit <dhrubajit1992@gmail.com>
2016-05-05 15:27:26 +0800

    added auto walk feature

--- Assets/Scripts/Navmesh.cs
+++ Assets/Scripts/Navmesh.cs
@@ -4,18 +4,17 @@ using System.Collections;
 public class Navmesh : MonoBehaviour {
 
 	NavMeshAgent agent;
-//	Vector3 pos = new Vector3(3.9f,1f,6f);
+	private float movementSpeed = 2;
 	private Vector3 newRoom = new Vector3(-2.62f,2f,5.43f);
 	private Vector3 startingPosition = new Vector3(5.22f,2f,-4.5f);
 	void Start () {
 		agent = GetComponent<NavMeshAgent>();
 	}
 
-	// Update is called once per frame
-//	void Update () {
-//		if ( Input.GetMouseButtonDown(1) ) {
-//				agent.SetDestination(pos);
-//		}
+
+	//for auto walk
+//	public void FixedUpdate(){
+//		GameObject.Find ("Capsule").transform.position += GameObject.Find ("Head").transform.forward * Time.deltaTime * movementSpeed;
 //	}
 
 	public void move(string pos){
--- Assets/Scripts/RadialProgressBar.cs
+++ Assets/Scripts/RadialProgressBar.cs
@@ -12,14 +12,7 @@ public class RadialProgressBar : MonoBehaviour, ICardboardGazeResponder {
 	[SerializeField] private float currentAmount;
 	[SerializeField] private float speed;
 	bool gazing = false;
-//	player = GameObject.FindGameObjectWithTag(Tags.player).transform;
-//	private Transform camera = GameObject.FindGameObjectWithTag("MainCamera").transform;
 
-	// Use this for initialization
-	void Start () {
-//		print (GameObject.Find ("CardboardMain").transform.position);
-	}
-	
 	// Update is called once per frame
 	void Update () {
 		if (gazing) {
@@ -31,12 +24,16 @@ public class RadialProgressBar : MonoBehaviour, ICardboardGazeResponder {
 		if (currentAmount >= 100) {
 			switch (style) {
 			case 1:
+				GameObject.Find ("Capsule").SetActive (false);
 				GameObject.Find ("CardboardMain").GetComponent <Jump> ().move (NewRoom);
 				break;
 			case 2:
+				GameObject.Find ("Capsule").SetActive (true);
 				GameObject.Find ("Capsule").GetComponent<Navmesh> ().move (NewRoom);
 				break;
+
 			default:
+				GameObject.Find ("Capsule").SetActive (false);
 				GameObject.Find ("CardboardMain").GetComponent <Jump> ().move (NewRoom);
 				break;
 			}
@@ -56,32 +53,3 @@ public class RadialProgressBar : MonoBehaviour, ICardboardGazeResponder {
 	public void OnGazeTrigger(){
 	}
 }
-
-//using UnityEngine;
-//using System.Collections;
-//
-//public class AgentScript : MonoBehaviour {
-//
-//	NavMeshAgent agent;
-//	// Use this for initialization
-//	void Start () {
-//		agent = GetComponent<NavMeshAgent>();
-//	}
-//
-//	// Update is called once per frame
-//	void Update () {
-//		if ( Input.GetMouseButtonDown(0) ) {
-//			Plane plane = new Plane(Vector3.up, 0);
-//			//Vector3 dest = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-//			float dist;
-//			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-//			if (plane.Raycast(ray, out dist))
-//			{
-//				Vector3 point = ray.GetPoint(dist);
-//				agent.SetDestination(point);
-//				//				indicator.position = point;
-//			}
-//		}
-//	}
-//}
-
