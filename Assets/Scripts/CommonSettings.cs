a918af768edd698ad1aa6379ee51021d2f1f614b
Luccan <luccan.ryanata@gmail.com>
2016-05-20 10:49:22 +0800

    Added targetFrameRate

---
+++ Assets/Scripts/CommonSettings.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CommonSettings : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        QualitySettings.vSyncCount = 0;  // VSync must be disabled
+        Application.targetFrameRate = 60;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        // might want to set targetFrameRate again here
+    }
+}
