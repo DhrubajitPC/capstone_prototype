143143a158eabbe2eb6ff11ace86bc082099d42e
Dhrubajit <dhrubajit1992@gmail.com>
2016-07-27 07:09:41 +0800

    add new scene to set asurl

---
+++ Assets/Scripts/sceneManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+//using UnityEditor;
+
+
+public class sceneManager : MonoBehaviour {
+
+	public void loadLayerScene(){
+		SceneManager.LoadScene("LayerScene");
+	}
+}
