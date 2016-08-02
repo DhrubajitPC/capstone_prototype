fb35e2014a82e108a9f82aab98488834b69c3a8a
Dhrubajit <dhrubajit1992@gmail.com>
2016-08-02 22:31:49 +0800

    added ambient light color to white and made capsule taller

--- Assets/Scripts/LayerMenuController.cs
+++ Assets/Scripts/LayerMenuController.cs
@@ -153,7 +153,6 @@ public class LayerMenuController : MonoBehaviour {
     // Update is called once per frame
     void Update () {
         Vector3 screenPoint = Camera.main.WorldToViewportPoint(canvas.transform.position);
-//		print (screenPoint);
         bool onScreen = screenPoint.z > 0 && screenPoint.x > 0 && screenPoint.x < 1 && screenPoint.y > 0 && screenPoint.y < 1;
         if (!onScreen)
         {
